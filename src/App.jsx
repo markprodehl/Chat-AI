9a577874ad50d8fe803530fea100dad7f6592f6a
Mark Prodehl <markprodehl@gmail.com>
2023-04-21 16:59:52 -0700

    Added a typing indicator to display while the ChatGPT api is responding

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,6 +8,7 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 function App() {
+  const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([
     {
       message: "Hello, I am ChatGPT",
@@ -20,11 +22,15 @@ function App() {
       sender: "user",
       direction: "outgoing"
     }
+
     const newMessages = [...messages, newMessage ] // All of thge old messages plus the new message
   
     // Update the message state
-    setMessages(newMessages)
+    setMessages(newMessages);
   
+    // Set a typing indicator (chatgpt is typing)
+    setTyping(true);
+
     // process the message to chatgpt (send it over the response)
   }
 
@@ -34,7 +40,10 @@ function App() {
       <div style={{ position: "relative", height: "800px", width: "700px"}}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
+            <MessageList
+              // If yping is set to true display the TypingIndicator element, otherwise null
+              typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing"/> : null}
+            >
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />
               })}
