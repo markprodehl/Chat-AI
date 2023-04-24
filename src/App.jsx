947589d7b1fdd968bd20efbb2a86bea501335518
Mark Prodehl <markprodehl@gmail.com>
2023-04-24 16:20:28 -0700

    Implemented functionality to process messages from the user to ChatGPT. It includes a function processMessageToChatGPT, making an API fetch call to gpt-3.5-turbo, and then processes the the formatting then printing the incoming data in a console.log. NOTE - You will need to add your own API_KEY to line 11 for the request to work.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
+// The APY_KEY will allow us to make calls to OpenAI from our personal account
+const API_KEY = "YOUR_API_KEY"
+
 function App() {
   const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([
@@ -24,16 +27,62 @@ function App() {
     }
 
     const newMessages = [...messages, newMessage ] // All of thge old messages plus the new message
-  
     // Update the message state
     setMessages(newMessages);
-  
     // Set a typing indicator (chatgpt is typing)
     setTyping(true);
-
-    // process the message to chatgpt (send it over the response)
+    // Process the message to chatgpt (send it over the response) with all the messages from our chat
+    await processMessageToChatGPT(newMessages);
   }
 
+  async function processMessageToChatGPT(chatMessages) {
+    // chatMessages looks like this { sender: "user" or "ChatGPT", message: "The message content here"}
+    // To send messages to the API we need to make a new API Array, which needs to be in this format for the frontend { role: "user", or "assistant", content: "The message content here" }
+    // To format the API data we can build a new array by mapping through the all of the chatMessages and create a new object
+    let apiMessages = chatMessages.map((messageObject) => {
+      // Define role as an empty string
+      let role = "";
+      if(messageObject.sender === "ChatGPT") {
+        role = "assistant"
+      } else {
+        role = "user"
+      }
+      return { role: role, content: messageObject.message}
+    });
+
+    // role: "user" -> message from the user
+    // role "assistant" -> message from ChatGPT
+    // role "system" -> one message defining how we want ChatGPT to talk. This is a really cool customization that most won't have access to
+    const systemMessage = {
+      role: "system",
+      content: "Explain all concepts like I am 10 years old." // You can also add things like "Speak like a pirate", or "Explain like I am a software engineer with 10 years of experience"
+    }
+
+    const apiRequestBody = {
+      "model" : "gpt-3.5-turbo",
+      "messages" : [
+        systemMessage, // Putting this at the front of the messages is require within the messages array to get processed
+        ...apiMessages // [message1, message2, message3]
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "post",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+      // Now we need to grab the data being returned from OpenAI
+    }).then((data) => {
+      // Return the data as a JSON object
+      return data.json();
+      // Another .then to allow us to grab the new JSOn data
+    }).then((data) => {
+      // For now just log this data
+      console.log(data);
+    })
+  }
 
   return (
    <div className="App">
@@ -49,12 +98,9 @@ function App() {
               })}
             </MessageList>
             <MessageInput placeholder="Type message here" onSend={handleSend} />
-
           </ChatContainer>
         </MainContainer>
-
       </div>
-
    </div>
   )
 }
