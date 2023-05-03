3036b4d8be742bde41fc37d0bcc503cecb52ca15
Mark Prodehl <markprodehl@gmail.com>
2023-05-03 13:07:12 -0700

    Moved the process message to chatGPT API request and the personality options choices to their own components

---
+++ src/components/PersonalityOptions.js
@@ -0,0 +1,10 @@
+const personalityOptions = [
+  { label: '10 Year Old', value: 'Explain all concepts like I am 10 years old.' },
+  { label: 'Comedian', value: 'Explain all concepts like I am a comedian.' },
+  { label: 'Pirate', value: 'Explain all concepts like I am a pirate.' },
+  { label: 'Empathetic', value: 'Explain all concepts like I am empathetic.' },
+  { label: 'Quirky', value: 'Explain all concepts with a lot of quirkiness' },
+  // Add more personality options here as needed
+];
+
+export default personalityOptions;
